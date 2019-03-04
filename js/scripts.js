562c56cb712e95621cc5da9dbbdba4c9534bd63c
amiinegal <you@example.com>
2019-03-04 12:07:12 +0300

    give an id for player1

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,18 @@ function rollDice1() {
   var player1 = document.getElementById("player1") ;
   var hold = document.getElementById("hold") ;
 var score = document.getElementById("score");
-var p1 = Math.floor(Math.random() * 6) + 1;
-var pt1 = Math.floor(Math.random() *6) +p1;
-var hod = Math.floor(Math.random() *0) +0;
+var p1 = Math.ceil(Math.random() * 6);
+var pt1 = Math.ceil(Math.random() *6);
+var hod = Math.ceil(Math.random() *0) +1;
 var total1 = p1 + pt1 ;
-player1.innerHTML = p1 && pt1 ;
-score.innerHTML = "You rolled"+p1+".";
-hold.innerHTML = total1;
-if (total1 > 100) {
+if(p1!==1){
+  return 0
+}else{
+  player1.innerHTML = p1 && pt1 ;
+  score.innerHTML = "You rolled"+p1+".";
+  hold.innerHTML = total1;
+}
+if (total1 > 11) {
   hold.innerHTML += "Hurrah!! you WIN";
 };
 };
@@ -17,15 +21,30 @@ function rollDice2() {
   var player2 = document.getElementById("player2") ;
   var hold = document.getElementById("hold") ;
   var score = document.getElementById("score");
-  var p2 = Math.floor(Math.random() * 6) + 1;
-  var pt2 = Math.floor(Math.random() * 6) + p2;
-  var hod = Math.floor(Math.random() *0) +0;
+  var p2 = Math.ceil(Math.random() * 6);
+  var pt2 = Math.ceil(Math.random() * 6);
+  var hod = Math.ceil(Math.random() *0) +0;
   var total2 = p2 + pt2;
-  player2.innerHTML = p2 && pt2 ;
-  score.innerHTML = "You rolled "+p2+".";
-  hold.innerHTML = total2;
-  if (total2 > 100) {
+  if(p2!=1){
+    return 0
+  }else {
+    player2.innerHTML = p2 && pt2 ;
+    score.innerHTML = "You rolled "+p2+".";
+    hold.innerHTML = total2;
+  }
+
+  if (total2 > 11) {
     hold.innerHTML += "Hurrah!! you've WON"+".";
   };
-
 };
+$(document).ready(function() {
+ $("form#input").submit(function(event){
+   event.preventDefault();
+   var new1 = $("#kal").val();
+   var new2 = $("#mint").val();
+
+   $("#jamila").text(new1);
+   $("#jamila2").text(new2);
+
+ });
+});
