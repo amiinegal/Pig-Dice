75469ac9ab705eea65a189a3c9d19b33e2479f06
amiinegal <you@example.com>
2019-03-04 14:22:58 +0300

    add jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -17,48 +17,62 @@
 //   hold.innerHTML += "Hurrah!! you WIN";
 // };
 // };
-    var roundScore = 0;
-    var gameScore = 0;
+  var roundScore1 = 0;
+  var gameScore1 = 0;
 function rollDice1() {
 var p1 = Math.ceil(Math.random() * 6);
     $("#player1").text(p1);
 if (p1 !==1) {
-  roundScore += p1;
+  roundScore1 += p1;
 } else {
-  roundScore = 0;
-}
+  roundScore1 = 0;
+};
 };
 
-function hold() {
-  gameScore += roundScore;
-  $("#score").text(gameScore);
+function hold1() {
+  gameScore1 += roundScore1;
+  $("#score1").text(gameScore1);
 };
 
-var roundScore = 0;
-var gameScore = 0;
+var roundScore2 = 0;
+var gameScore2 = 0;
 function rollDice2() {
 
   var p2 = Math.ceil(Math.random() * 6);
   $('#player2').text(p2);
   if (p2 !==1) {
-    roundScore += p2
-  }else {
-    roundScore = 0;
+    roundScore2 += p2
+  console.log(roundScore2)
+  }
+  else {
+    roundScore2-=roundScore2;
   }
 };
 
-function hold() {
-  gameScore += roundScore;
-  $("#score").text(gameScore);
+
+function hold2() {
+  console.log(roundScore2)
+  gameScore2 += roundScore2;
+  $("#score2").text(gameScore2);
 };
 
 $(document).ready(function() {
   $("#roll1").click(function() {
     rollDice1();
+    $("#current1").text(roundScore1)
   });
-  $("#hold").click(function() {
-    hold();
+  $("#hold1").click(function() {
+    hold1();
   });
+
+  $("#roll2").click(function(){
+    rollDice2();
+
+    $("#current2").text(roundScore2)
+  });
+  $("#hold2").click(function() {
+    hold2();
+  })
 
  $("form#input").submit(function(event){
    event.preventDefault();
